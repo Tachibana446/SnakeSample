cda5147e6cb899ca0d2127dec07c736cc644ef19
Tachibana446 <stinger33z@gmail.com>
2014-10-24 01:27:57 +0900

    最初のコミット

---
+++ src/snake/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package snake;
+
+/**
+ * @author Tachibana446
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
